d403496a5b67ea37c936eb7893bc226ba6ddcdef
zhuheyang <271063884@qq.com>
2017-10-23 20:58:45 +0800

    init commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+export class Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>{{title}}</h1>
+    <h2>{{hero.name}} details!</h2>
+    <div><label>id: </label>{{hero.id}}</div>
+    <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+    </div>
+    `
+  // styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'My First Angular App';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+}
+
+
+
